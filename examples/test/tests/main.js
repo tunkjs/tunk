4d487e01702493ad3f77298c683a6f6b3233f62a
mac <522702986@qq.com>
2017-05-16 18:32:29 +0800

    .

--- examples/test/tests/main.js
+++
@@ -1,8 +0,0 @@
-
-export default {
-	options:require('./options.vue'),
-	promise:require('./promise.vue'),
-	request:require('./request.vue'),
-	//cookie:require('./cookie.vue'),
-	//function:require('./function.vue'),
-}
